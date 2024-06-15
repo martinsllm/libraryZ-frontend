f489d31174f8bcba1bb9de5cde5560e9c50318ed
martinsllm <lucaslm2010@gmail.com>
2024-06-15 00:57:15 -0300

    Creating access page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Button, Container, Form, Input } from "reactstrap";
+import './index.css'
+import Logo from "./assets/image.jpg";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> Hello Darkness! </p>
-      </header>
-    </div>
+    <Container className="container">
+        <Form className="col-md-5 form">
+          <img src={Logo} alt="Airbnb logo" />
+          <Input type="email" placeholder="E-mail"></Input>
+          <Input type="password" placeholder="Senha"></Input>
+          <Button>Entrar</Button>
+        </Form>
+    </Container>
   );
 }
 
