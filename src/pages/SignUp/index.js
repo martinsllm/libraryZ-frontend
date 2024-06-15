7d2125105ae9aa91216a57725e0100afce12cb94
martinsllm <lucaslm2010@gmail.com>
2024-06-15 19:23:48 -0300

    Implementing user's register

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,18 +2,62 @@ import React, { Component } from 'react';
 import { Container, Form, Input, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/image.jpg';
+import api from '../../services/api';
 import '../../styles/style.css';
 
 class SignUp extends Component {
+    state = {
+        email: '',
+        password: '',
+        confPassword: '',
+        error: ''
+    }
+
+    handleSignUp = async e => {
+        e.preventDefault();
+        const { email, password, confPassword } = this.state;
+
+        if (!email || !password || !confPassword) {
+            this.setState({ error: "Preencha todos os dados para se cadastrar!" });
+        } else {
+            try {
+                await api.post('/user', { email, password }) 
+                this.reload()
+            } catch (error) {
+                this.setState({ error: "Ocorreu um erro ao registrar sua conta. T.T" });
+            }
+        }
+
+    }
+
+    reload() {
+        setTimeout(() => {
+            window.location.href = '/'
+        }, 1000)
+    }
+
     render() {
         return (
             <Container className="container">
-                <Form className="form col-md-5">
+                <Form className="form col-md-5" onSubmit={this.handleSignUp}>
                     <img src={Logo} alt="LibraryZ logo" />
-                    <Input type="email" placeholder="E-mail"></Input>
-                    <Input type="password" placeholder="Senha"></Input>
-                    <Input type="password" placeholder="Confirme sua senha"></Input>
-                    <Button>Cadastrar</Button>
+                    {this.state.error && <p>{this.state.error}</p>}
+                    <Input 
+                        type="email" 
+                        placeholder="E-mail"
+                        onChange={e => this.setState({ email: e.target.value })}
+                    />
+                    <Input 
+                        type="password" 
+                        placeholder="Senha" 
+                        onChange={e => this.setState({ password: e.target.value })}
+                    />
+                    <Input 
+                        type="password" 
+                        placeholder="Confirme sua senha"
+                        onChange={e => this.setState({ confPassword: e.target.value })}
+                    />
+                    <Button type="submit">Cadastrar</Button>
                     <hr/>
                     <Link to="/">Fazer login</Link>
                 </Form>
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes, Navigate } from "react-router-dom";
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = () => {
+    return isAuthenticated() ? <Outlet /> : <Navigate to="/"/>
+}
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
             <Route exact path='/' element={<SignIn />}/>
             <Route exact path='/signup' element={<SignUp />}/>
+            <Route exact path='/app' element={<PrivateRoute> <h1>Welcome to LibraryZ</h1> </PrivateRoute>}/>
             <Route path='*' element={<h1>Page Not Found</h1>}/>
         </Routes>
     </BrowserRouter>
