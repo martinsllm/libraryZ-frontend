42bd6001c84df7c8e0cf9bac2348139d2fbaa166
martinsllm <lucaslm2010@gmail.com>
2024-06-28 20:51:12 -0300

    Changing Header to Hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   Collapse,
   Navbar,
@@ -12,48 +12,41 @@ import { IoMdExit } from 'react-icons/io';
 import { FaTicket } from "react-icons/fa6";
 import { logout } from '../services/auth';
 
-export default class Header extends Component {
-  constructor(props) {
-    super(props);
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
+  const toggle = () => {
+    setIsOpen(!isOpen)
   }
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-  logout() {
+  const exit = () => {
     logout();
     window.location.href = '/';
   }
 
-  render() {
-    return (
-      <div>
-        <Navbar color="dark" dark expand="md">
-          <NavbarBrand href="/home">LIbraryZ</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/cart"><FaShoppingCart className="mb-1"/> Meu Carrinho</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/sales"><FaTicket className="mb-1"/> Acompanhar Pedidos </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink onClick={this.logout} style={{cursor: "pointer"}}><IoMdExit className="mb-1"/> Sair </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+  
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/home">LIbraryZ</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/cart"><FaShoppingCart className="mb-1"/> Meu Carrinho</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/sales"><FaTicket className="mb-1"/> Acompanhar Pedidos </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink onClick={exit} style={{cursor: "pointer"}}><IoMdExit className="mb-1"/> Sair </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
+
+
+export default Header;
